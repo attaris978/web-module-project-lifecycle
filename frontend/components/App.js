7f2c85a43f9a77625bb515ae1b8bc1a3036963e8
unknown <ryancare@protonmail.com>
2022-04-12 10:32:56 -0500

    Added mount-time fetch request to API with error handling; constructors for App and Form state; added styled-components for later form-wrapper styling

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,7 +3,26 @@ import React from 'react'
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      data:[]
+    }
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(json => this.setState( {
+      data: json.data
+    } ) )
+    .catch(error => console.log(error))
+  }
   render() {
-    return null
+    return (
+              <h1>Hiya</h1>
+
+    )
   }
 }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './TodoList';
+import styled from 'styled-components';
+
+const FormWrapper = styled.form`
+
+`;
 
 export default class Form extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      todoText: ''
+    }
+  }
+
+
   render() {
-    return null
+    return (
+      
+    )
   }
 }
